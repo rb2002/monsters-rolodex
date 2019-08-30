5e0b60161448e51756bae9c7740058d28ed325a4
Ray Bard <rayban_250@hotmail.com>
2019-08-29 22:52:05 -0400

    Updates

---
+++ precache-manifest.3d4011b0a4ed51aca49ffebe7f78931e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74152e405c9664fec69e30d1567a2dcd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "326ed6935fb6b82c4bde",
+    "url": "/monsters-rolodex/static/css/main.86315558.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "326ed6935fb6b82c4bde",
+    "url": "/monsters-rolodex/static/js/main.54c478ab.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
